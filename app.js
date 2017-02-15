2e2d085d7fed586b04d1b5550c3baa9dc048d79d
AKalyani92 <aditikalyani92@gmail.com>
2017-02-15 12:14:28 +0530

    change id pass

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' });
+({ appId: '7a5470f5-fb9f-43e0-85c5-eb764131728e', appPassword: 'vPmda5yurk8uiO7tT9nbYah' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 

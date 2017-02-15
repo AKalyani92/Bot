c9996ac160bb0aac4154a4766df7896bf3d501a2
AKalyani92 <aditikalyani92@gmail.com>
2017-02-15 11:33:40 +0530

    New app

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-/**
- * Created by aditi.kalyani on 15-02-2017.
- */
+// Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
-//=========================================================
-// Bot Setup
-//=========================================================
-
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.PORT || 3000, function()
+{
     console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector({
-    appId: "3f4a79f0-c5cb-4f68-a9fe-4ab3f690fb28",
-    appPassword: "RjSHX1iLG2UhrPcybjxPbTK"
-});
+var connector = new builder.ChatConnector
+({ appId: '3f4a79f0-c5cb-4f68-a9fe-4ab3f690fb28', appPassword: 'RjSHX1iLG2UhrPcybjxPbTK' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
+// Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });

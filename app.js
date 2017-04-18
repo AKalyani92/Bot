b56312b300d35dd32f04907b0934b6c5ddc467a4
AKalyani92 <aditikalyani92@gmail.com>
2017-04-18 07:54:20 +0530

    Switch case added

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ bot.dialog('/', [
             session.send("Not Trained...");
         }
     }
+    /*function (session, results) {
+        session.endConversation("Goodbye until next time...");
+    }*/
 ]);
 
 bot.dialog('/payment', [
@@ -52,8 +55,64 @@ bot.dialog('/payment', [
         session.send(results.response);
         session.send("OTP Verified. Thank You");
         session.send("Your last payment invoice is: OPEN as of 04/14/2017");
+        session.beginDialog('rootMenu');
         // builder.Prompts.text(session,"How can i help you?");
-        // builder.Prompts.choice(session, "", "Payment Amount|Date of Payment|Payment Mode|Payment method|Bank", { listStyle: builder.ListStyle.none })
+        // builder.Prompts.choice(session, "", "Payment Amount|Date of Payment|Payment Mode|Payment method|Bank|Main Menu", { listStyle: builder.ListStyle.button })
+    }
+  /*  function (session, results) {
+        switch (results.response.index) {
+            case 0:
+                session.send("Amount is: $ 15,000");
+                break;
+            case 1:
+                session.send("Date is: $ 09/04/2017");
+                break;
+            case 2:
+                session.send("Payment Mode is: DD");
+                break;
+            case 3:
+                session.send("Payment Method is: DD");
+                break;
+            case 4:
+                session.send("Payment Bank is: ICICI");
+                break;
+            case 5:
+                session.endDialog();
+                break;
+        }
+    }*/
+]);
+
+bot.dialog('rootMenu', [
+    function (session) {
+        builder.Prompts.choice(session, "Please select an option", "Payment Amount|Date of Payment|Payment Mode|Payment method|Bank|Main Menu", { listStyle: builder.ListStyle.button })
+    },
+    function (session, results) {
+        switch (results.response.index) {
+            case 0:
+                session.send("Amount is: $ 15,000");
+                session.replaceDialog('rootMenu');
+                break;
+            case 1:
+                session.send("Date is: $ 09/04/2017");
+                session.replaceDialog('rootMenu');
+                break;
+            case 2:
+                session.send("Payment Mode is: DD");
+                session.replaceDialog('rootMenu');
+                break;
+            case 3:
+                session.send("Payment Method is: DD");
+                session.replaceDialog('rootMenu');
+                break;
+            case 4:
+                session.send("Payment Bank is: ICICI");
+                session.replaceDialog('rootMenu');
+                break;
+            case 5:
+                session.endDialog();
+                break;
+        }
     }
 ]);
 

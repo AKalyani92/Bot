76e7b981fd0016e515a12c9b34e4168b206f76be
AKalyani92 <aditikalyani92@gmail.com>
2017-04-18 19:46:19 +0530

    Issue added

--- app.js
+++ app.js
@@ -30,8 +30,13 @@ bot.dialog('/', [
         } else if (results.response.toUpperCase().indexOf("INVENTORY") !== -1) {
             // session.send("UPDATE");
             session.beginDialog('/inventory');
+        }
+        else if (results.response.toUpperCase().indexOf("ISSUE") !== -1) {
+            // session.send("UPDATE");
+            session.beginDialog('/issues');
         } else {
             session.send("Not Trained...");
+            session.endDialog();
         }
     }
     /*function (session, results) {
@@ -137,6 +142,32 @@ bot.dialog('/inventory', [
     }
 ]);
 
+bot.dialog('/issues', [
+    function (session) {
+        builder.Prompts.text(session, 'Please Enter Your DUNS number');
+    },
+    function (session, results) {
+        builder.Prompts.text(session, "An OTP has been sent to the registered email on file. Please Enter the OTP.");
+    },
+    function (session,results) {
+        session.send("OTP Verified. Thank You");
+        builder.Prompts.text(session, 'Is this an existing issue or a new issue?');
+    },
+    function (session, results) {
+        if (results.response.toUpperCase().indexOf("NEW") !== -1) {
+            session.beginDialog('newIssue');
+        }
+        else
+        {
+            session.beginDialog('existingIssue');
+        }
+        //session.beginDialog('inventoryMenu');
+        // builder.Prompts.text(session,"How can i help you?");
+        // builder.Prompts.choice(session, "", "Payment Amount|Date of Payment|Payment Mode|Payment method|Bank|Main Menu", { listStyle: builder.ListStyle.button })
+    }
+]);
+
+
 bot.dialog('inventoryMenu', [
     function (session) {
         builder.Prompts.choice(session, "The following products are supplied by you. Choose one to know more.", "Silicon Wafer 200 micron|Galvanized metal mesh 10*20|Polished Silicon Wafer 2 inches|Plain Weave screen 600 microns|Steel Cable wire 2 mm|Main Menu", { listStyle: builder.ListStyle.button })
@@ -169,4 +200,33 @@ bot.dialog('inventoryMenu', [
         }
     }
 ]);
+bot.dialog('newIssue', [
+    function(session) {
+        //session.send("OK! You have selected new issue, Please describe your issue in few words");
+        builder.Prompts.text(session, 'OK! You have selected new issue, Please describe your issue in few words');
+    },
+    function (session, results) {
+        builder.Prompts.text(session, 'what is priority (1-5)');
+    },
+    function (session, results) {
+        session.send('Great! new issue created.Your issue key is 1234.');
+        session.endDialog();
+    }
+]);
 
+bot.dialog('existingIssue', [
+    function (session) {
+        builder.Prompts.choice(session, "Ok! Please select one of the following issues", "VI-123|VI-234|VI-346", { listStyle: builder.ListStyle.button })
+    },
+    function (session, results) {
+        session.send("Following are the details of your issue");
+        session.send("Ticket No : "+ results.response.entity);
+        session.send("Summary   : Incorrect vendor contact details");
+        session.send("Priority   : 3");
+        builder.Prompts.text(session, "would you like to escalate?");
+    },
+    function (session, results) {
+        session.send("Ok! Thank you");
+        session.endDialog();
+    }
+]);
